111e70474944365933074bfef04c21c222a2805d
teeboude <marceldu03@gmail.com>
2023-02-21 12:40:24 +0100

    Merge branch 'developp' of D:\CGI\formation Java Script Angular AJC\projets_intelliJ\Projet-Angular-Events with conflicts.

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Event} from "../models/event"
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  fetchEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>('http://localhost:3000/events')
+  }
+
+  createEvent(createProduct: any): Observable<void> {
+    return this.http.post<void>('http://localhost:3000/events', createProduct);
+  }
 }
